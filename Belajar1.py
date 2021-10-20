c1e6668759ede23426d5e9812d05a040c0bbcdb9
unknown <imansetiawan1998@gmail.com>
2021-10-20 23:46:11 +0700

    main

---
+++ Belajar1.py
@@ -0,0 +1,12 @@
+print ("Belajar Input Perkenalan Menggunakan Python ")
+print ()
+nama = input("Nama Lengkap :")
+panggilan = input("Nama Panggilan :")
+alamat = input ("Alamat :")
+umur = input ("Umur :")
+hobi = input ("Hobi :")
+print ()
+print ("Hallo nama saya",nama,"biasa di panggil",panggilan,"saya tinggal di"
+        ,alamat,"umur saya",umur,"hobi saya itu",hobi)
+print ()
+print ("Salam Kenal ya")
